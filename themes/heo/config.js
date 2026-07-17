48ff1a1da912ce39ec056c8944a0c52253b4cf93
yrygsxm <76896047+yrygsxm@users.noreply.github.com>
2026-07-17 22:08:40 +0900

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -53,8 +53,8 @@ const CONFIG = {
   ],
 
   // 个人资料底部按钮
-  HEO_INFO_CARD_URL1: '/about',
-  HEO_INFO_CARD_ICON1: 'https://song.al/',
+  HEO_INFO_CARD_URL1: 'https://song.al/',
+  HEO_INFO_CARD_ICON1: 'fas fa-user',
   HEO_INFO_CARD_URL2: 'https://appdo.xyz/article/about',
   HEO_INFO_CARD_ICON2: 'fab fa-github',
   HEO_INFO_CARD_URL3: 'https://about.appdo.xyz/',
